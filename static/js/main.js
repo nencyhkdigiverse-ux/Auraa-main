d83d02da986597a8276b0fe5dd2c30a964b15bf0
Nency Savaliya <nencysavaliya5@gmail.com>
2026-02-25 12:54:06 +0530

    changes in hero section

--- static/js/main.js
+++ static/js/main.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
         const frameCount = 40;
-        const currentFrame = index => `/static/bhavin/ezgif-frame-${(index + 1).toString().padStart(3, '0')}.jpg`;
+        const currentFrame = index => `/static/Gauu maata/eg-${(index + 1).toString().padStart(3, '0')}.jpg`;
 
         const images = [];
         const airpods = {
